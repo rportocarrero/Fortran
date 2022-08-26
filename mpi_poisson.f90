4394ac7b9cdebc9261603c0be92782e54e9550fa
rportocarrero <portocarrero.ryan@gmail.com>
2022-08-26 10:42:05 -0400

    Update mpi_poisson.f90
    modified for testing

--- mpi_poisson.f90
+++ mpi_poisson.f90
@@ -1,62 +1,98 @@
 program poisson
 
     implicit none
+    include "mpif.h"
 
-    integer, parameter :: GRIDSIZE=10
-    integer ierr
-    
-    real u(0:(GRIDSIZE+1),unew(0:(GRIDSIZE+1))
-    real rho(0:(GRIDSIZE+1))
+    integer, parameter :: u(:), unew(:)
+    real, allocatable :: rho(:)
+    integer i 
+    integer my_rank, n_ranks, ierr
+    integer my_j_max
+
     real h, hsq
-    double precision unorm, residual
+    double precision unorm, difference
+
+    call MPI_Init(ierr)
+
+    call MPI_Comm_rank(MPI_COMM_WORLD, my_rank, ierr)
+    call MPI_Comm_size(MPI_COMM_WORLD, n_ranks, ierr)
+
+    my_j_max = GRIDSIZE/n_ranks;
+
+    real u(0:(my_j_max+1)), unew(0:(my_j_max+1))
+    real rho(0:(my_j_max+1))
 
     h = 0.1
-    hsq = h * h
-    residual = 1e-5
+    hsq = h*h
+
+    do i = 0, my_j_max+1
+        u(i) = 0.0
+        rho(i) = 0.0
+    enddo
+
+    call poisson_step(u, unew, rho, my_j_max, hsq, unorm)
+
+    if(unorm == 25) then
+        write(6,*) "PASSED after 1 step"
+    else
+        write(6,*) "FAILED after 1 step"
+        write(6,*) unorm
+    end if
+
+    do i = 1, 10
+        call poisson_step(u, unew, rho, my_j_max, hsq, unorm)
+    end do
+
+    difference = unorm - 0.40042400360107422
+    if (difference*difference < 1e-16) then
+        write(6,*) "PASSED after 10 steps"
+    else
+        write(6,*) "FAILED after 10 steps"
+        write(6,*) unorm
+    end if
+
+    call MPI_Finalize(ierr)
+
+subroutine poisson_step(u,unew,rho,GRIDSIZE,hsq,unorm)
+
+    implicit none
+
+    integer, parameter :: GRIDSIZE=10
+    integer i
+
+    real u(0:(GRIDSIZE+1)), unew(0:(GRIDSIZE+1))
+    real rho(0:(GRIDSIZE+1))
+    real h, hsq
+    double precition unorm, difference
+
+    h = 0.1
+    hsq = h*h
 
     do i = 0, GRIDSIZE+1
         u(i) = 0.0
         rho(i) = 0.0
     enddo
 
-    u(0) = 10.0
+    u(0) = 10
 
-    do
+    call poisson_step(u, unew, rho, GRIDSIZE, hsq, unorm)
+
+    if (unorm == 25) then
+        write(6, *) "PASSED afer 1 step"
+    else
+        write(6,*) "FAILED after 1 step"
+        write(6, *) unorm
+    end if
+
+    do i=1, 10
         call poisson_step(u, unew, rho, GRIDSIZE, hsq, unorm)
-        write(6,*) 'Iteration', i, ', residual', unorm
+    end do
 
-        if(sqrt(unorm) <= sqrt(residual)) then
-            exit
-        end if
-
-    enddo
-
-    write(6,*) 'Run completed with residual ', unorm
-
-end
-
-subroutine poisson_step(u,unew,rho,GRIDSIZE,hsq,unorm)
-
-    implicit none
-
-    integer, intent(in) :: GRIDSIZE
-    real, intent(inout), dimension (0:(GRIDSIZE+1)) :: u, unew
-    real, intent(in), dimension (0:(GRIDSIZE+1)) :: rho
-    real, intent(in) :: hsq
-    double precision, intent(out) :: unorm
-    intger i, j
-
-    do i=1, GRIDSIZE
-        unew(i) = 0.25*(u(i-1)+u(i+1)-hsq*rho(i))
-    enddo
-
-    unorm = 0.0
-
-    do i = 1, GRIDSIZE
-        unorm = unorm + (unew(i)-u(i))*(unew(i)-u(i))
-    enddo
-
-    do i = 1, GRIDSIZE
-        u(i) = unew(i)
-    enddo
+    difference = unorm = 0.400442400360107422
+    if (difference*difference < 1e-16) then
+        write(6, *) "PASSED after 10 steps"
+    else
+        write(6, *) "FAILED after 10 steps"
+        write(6, *) unorm
+    end if
 end
